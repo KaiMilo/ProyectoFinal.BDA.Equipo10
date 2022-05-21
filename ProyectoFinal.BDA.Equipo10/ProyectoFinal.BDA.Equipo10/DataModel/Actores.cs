4a6097bf3ac56946d5c6fdcc57e16bd015ed30fb
Alan-Bg <44452311+Alan-Bg@users.noreply.github.com>
2022-05-21 14:50:03 -0500

    Update Actores.cs

--- ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actores.cs
+++ ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actores.cs
@@ -1,5 +1,7 @@
 ﻿namespace ProyectoFinal.BDA.Equipo10.DataModel
 {
+//Creamos la clase llamada Actor con los atributos de Nombre, Foto y Desc
+
     public class Actor
     {
         public string Nombre { get; set; }

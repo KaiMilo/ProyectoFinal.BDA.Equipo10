2efafbc9aab557aa60370442cdb1d7ff1abdeb11
Alan-Bg <44452311+Alan-Bg@users.noreply.github.com>
2022-05-21 21:30:12 -0500

    Update Actrices.cs

--- ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actrices.cs
+++ ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actrices.cs
@@ -7,6 +7,16 @@
         public string Nombre { get; set; }
         public string Foto { get; set; }
         public string Desc { get; set; }
+        
+        
+        public string DescLarga9 { get; set; }
+        public string DescLarga10 { get; set; }
+        public string DescLarga11 { get; set; }
+        public string DescLarga12 { get; set; }
+        public string DescLarga13 { get; set; }
+        public string DescLarga14 { get; set; }
+        public string DescLarga15 { get; set; }
+        public string DescLarga16 { get; set; }
 
     }
 }

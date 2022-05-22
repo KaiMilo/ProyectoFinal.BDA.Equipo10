76cca1d06d15c84412ac6027985ed822a5501bba
KaiMilo <62195892+KaiMilo@users.noreply.github.com>
2022-05-22 18:26:54 -0500

    Add files via upload

---
+++ ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/Pages/Descripcion-Actriz.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ProyectoFinal.BDA.Equipo10.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinal.BDA.Equipo10.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Descripcion_Actriz : ContentPage
+    {
+        public Descripcion_Actriz(Actriz act)
+        {
+            InitializeComponent();
+            Actriz act1 = act;
+
+
+            //SE ponen los datos correspondientes para que se muestre el actor en la pantalla
+            Nombre.Text = act1.Nombre;
+            Image.Source = act1.Foto;
+            Desc.Text = act1.Desc;
+        }
+    }
+}

bb0a2789f95949259f7f03b59ab50368094bc972
Alan-Bg <44452311+Alan-Bg@users.noreply.github.com>
2022-05-21 21:29:12 -0500

    Update Actores.cs

--- ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actores.cs
+++ ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/DataModel/Actores.cs
@@ -7,5 +7,14 @@
         public string Nombre { get; set; }
         public string Foto { get; set; }
         public string Desc { get; set; }
+        
+        public string DescLarga1 { get; set; }
+        public string DescLarga2 { get; set; }
+        public string DescLarga3 { get; set; }
+        public string DescLarga4 { get; set; }
+        public string DescLarga5 { get; set; }
+        public string DescLarga6 { get; set; }
+        public string DescLarga7 { get; set; }
+        public string DescLarga8 { get; set; }
     }
 }

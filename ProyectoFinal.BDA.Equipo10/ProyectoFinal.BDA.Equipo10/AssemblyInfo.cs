fd3cf608055c63749df254223b9a2ebb9e75736d
KaiMilo <62195892+KaiMilo@users.noreply.github.com>
2022-05-22 19:46:07 -0500

    Update AssemblyInfo.cs

--- ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/AssemblyInfo.cs
+++ ProyectoFinal.BDA.Equipo10/ProyectoFinal.BDA.Equipo10/AssemblyInfo.cs
@@ -3,6 +3,8 @@ using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
+//Exportamos las fuentes que vamos usar en el proyecto
+
 [assembly: ExportFont("Baloo2-ExtraBold.ttf",Alias = "Baloo2-EB")]
 [assembly: ExportFont("Baloo2-VariableFont_wght.ttf", Alias ="Baloo2-Normal")]
 [assembly: ExportFont("BalooDa2-VariableFont_wght.ttf", Alias ="BalooDa2-Normal")]
